24e6e9349bea75b18f40cd49a18fd7b9faa5571c
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2022-02-03 17:10:27 +0900

    added Classes Achievements, Location and their attributions

---
+++ AchieveNow/AchieveNow/Classes/Achievements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AchieveNow.Classes
+{
+    public class Achievements
+    {
+        [MaxLength(300)]
+        private string Name { get; set; }
+        
+        [Range(1950, 2100)]
+        private DateTime Dateofissue { get; set; }
+        
+        [Range(0, 3)]
+        private byte Results { get; set; }
+    }
+}
---
+++ AchieveNow/AchieveNow/Classes/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AchieveNow.Classes
+{
+    public class Location
+    {
+        [MaxLength(300)]
+        private string Place { get; set; }
+        
+    }
+}
