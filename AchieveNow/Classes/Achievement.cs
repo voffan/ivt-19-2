d49325fb01d03e269792e0e685171b7306537fb2
nm-klimov <blarc7@gmail.com>
2022-02-09 21:55:23 +0900

    changed competition and achievement

--- AchieveNow/Classes/Achievement.cs
+++ AchieveNow/Classes/Achievement.cs
@@ -15,5 +15,7 @@ namespace AchieveNow.Classes
         public string? Name { get; set; }
         public DateTime DateOfIssue { get; set; }
         public byte Results { get; set; }
+        public int CompetitionId { get; set; }
+        public virtual Competition? Competition { get; set; }
     }
 }
