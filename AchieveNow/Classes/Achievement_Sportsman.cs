a3468ca48fac1871d2c00a2bef5499139171f517
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2022-02-09 21:45:20 +0900

    added new classes

---
+++ AchieveNow/Classes/Achievement_Sportsman.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AchieveNow.Classes
+{
+    public class Achievement_Sportsman
+    {
+        public int AchievementId { get; set; }
+        public virtual Achievement? Achievement { get; set; }
+        public int SportsmanId { get; set; }
+        public virtual Sportsman? Sportsman { get; set; }
+    }
+}
