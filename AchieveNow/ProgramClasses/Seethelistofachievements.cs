a286099ad880eebc4f88dd47a6a79961eb8bc135
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2022-02-23 23:12:57 +0900

    Changed case of Achievements

--- AchieveNow/ProgramClasses/Seethelistofachievements.cs
+++ AchieveNow/ProgramClasses/Seethelistofachievements.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace AchieveNow.ProgramClasses
 {
     public class Seethelistofachievements
     {
+        ApplicationContext context = new ApplicationContext();
+       // var achievements = context.Achievements();
+
 
     }
 }
