1ed1be7f03c9882c6294860e7ae3e446fabd9f69
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2022-02-24 01:55:25 +0900

    added See the list of achievements

--- AchieveNow/ProgramClasses/SeeTheList/Seethelistofachievements.cs
+++ AchieveNow/ProgramClasses/SeeTheList/Seethelistofachievements.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using AchieveNow.Classes;
 using Microsoft.EntityFrameworkCore;
 
 namespace AchieveNow.ProgramClasses
 {
-    public class Seethelistofachievements
+    public class SeeTheListOfAchievements
     {
-        ApplicationContext context = new ApplicationContext();
-       //var achievements = context.Achievements();
+        public void ShowAchievements()
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                string listAchievements = "Список достижений: \n";
+                var achievements = context.Achievements
+                    .Include(a => a.Competition)
+                    .ToList();
+                foreach (Achievement achievement in achievements)
+                {
+                    listAchievements += $"{achievement.Id}. {achievement.Name}; Дата проведения - {achievement.DateOfIssue}; Результат - {achievement.Results}; Мероприятие - {achievement.Competition.Name}\n";
+                }
 
-
+                MessageBox.Show(listAchievements);
+            }
+        }
     }
 }
