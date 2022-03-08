018779d5aa447dd4b75c59b00e9e4fc72107a334
Монастырев Ньургун <Nurgun.Monastyrev.2001@gmail.com>
2022-03-08 23:07:42 +0900

    Context

--- Yaxel/Yaxel/Form1.cs
+++ Yaxel/Yaxel/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Yaxel.Classes;
 
 namespace Yaxel
 {
@@ -15,6 +16,22 @@ namespace Yaxel
         public Form1()
         {
             InitializeComponent();
+
+            using (var context = new YaxelContext())
+            {
+                var employee = new Employee();
+                {
+                    employee.Name = "Monastyrev Nurgun";
+                    employee.Login = "ZeMonast";
+                    employee.Password = "qwerty";
+                    employee.Position = Position.Sysadmin;
+                }
+
+                context.Employees.Add(employee);
+                context.SaveChanges();
+
+                label1.Text = $"Id: {employee.Id}, Name: {employee.Name}";
+            }
         }
     }
 }
---
+++ Yaxel/Yaxel/YaxelContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Yaxel.Classes;
+using System.Data.Entity;
+
+namespace Yaxel
+{
+    internal class YaxelContext : DbContext
+    {
+        public YaxelContext() : base ("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Computer> Computers { get; set; }
+        public DbSet<Periphery> Peripheries { get; set; }
+        public DbSet<Component> Components { get; set; }
+        public DbSet<Classes.Attribute> Attributes { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
