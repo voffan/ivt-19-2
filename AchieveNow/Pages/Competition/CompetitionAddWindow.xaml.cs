8e776c1b6d37606824c19815983470f7556b46e2
sherwood2142 <sherwood2142@icloud.com>
2022-03-22 02:48:55 +0900

    added the ability to select Location and SportKind. Working on validation of entered data

--- AchieveNow/Pages/Competition/CompetitionAddWindow.xaml.cs
+++ AchieveNow/Pages/Competition/CompetitionAddWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AchieveNow.Classes;
+using AchieveNow.ProgramClasses;
+using Microsoft.EntityFrameworkCore;
 
 namespace AchieveNow.Pages.Competition
 {
@@ -24,8 +27,97 @@ namespace AchieveNow.Pages.Competition
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                var locations = context.Locations.ToList();
+
+                foreach (Location location in locations)
+                {
+                    Location_ComboBox.Items.Add(location);
+                }
+
+                Location_ComboBox.DisplayMemberPath = "Name";
+                Location_ComboBox.SelectedValuePath = "Id";
+
+                var sportKinds = context.SportKinds.ToList();
+
+                foreach (SportKind sportKind in sportKinds)
+                {
+                    SportKind_ComboBox.Items.Add(sportKind);
+                }
+
+                SportKind_ComboBox.DisplayMemberPath = "Name";
+                SportKind_ComboBox.SelectedValuePath = "Id";
+            }
+        }
+
+        private void Location_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageBox.Show(Location_ComboBox.SelectedValue.ToString());
+        }
+
+        private void SportKind_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageBox.Show(SportKind_ComboBox.SelectedValue.ToString());
+        }
+
+        private void DateOfExecution_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            /*DateTime? dateOfExecution = DateOfExecution.SelectedDate;
+            if (dateOfExecution >= DateTime.Today)
+            {
+                MessageBox.Show(dateOfExecution.Value.Date.ToShortDateString());
+            }
+            else
+            {
+                MessageBox.Show("Неправильно выбрана дата!");
+            }*/
+        }
+
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
+            if (Name.Text == "")
+            {
+                MessageBox.Show("В названии пусто");
+            }
+
+            /*if (Location_ComboBox.SelectedValue != null && Int32.TryParse(Location_ComboBox.SelectedValue.ToString(), out int locationId))
+            {
+                MessageBox.Show(locationId.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Ничего не выбрано");
+            }
+
+            if (SportKind_ComboBox.SelectedValue != null && Int32.TryParse(SportKind_ComboBox.SelectedValue.ToString(), out int sportKindId))
+            {
+                MessageBox.Show(sportKindId.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Ничего не выбрано");
+            }*/
+
+            if (DateOfExecution.SelectedDate.ToString() != "")
+            {
+                DateTime? dateOfExecution = DateOfExecution.SelectedDate;
+                if (dateOfExecution >= DateTime.Today)
+                {
+                    MessageBox.Show(dateOfExecution.Value.Date.ToShortDateString());
+                }
+                else
+                {
+                    MessageBox.Show("Нельзя выбрать прошлые даты");
+                }
+            } 
+            else
+            {
+                MessageBox.Show("Дата не выбрана");
+            }
+
             this.DialogResult = true;
         }
     }
