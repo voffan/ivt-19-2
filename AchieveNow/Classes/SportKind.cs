c910ad0bae76306026cca99c01c4ff65fb990a33
sherwood2142 <sherwood2142@icloud.com>
2022-03-22 20:10:46 +0900

    bit changes

--- AchieveNow/Classes/SportKind.cs
+++ AchieveNow/Classes/SportKind.cs
@@ -13,12 +13,14 @@ namespace AchieveNow.Classes
         public int Id { get; set; }
         [MaxLength(50)]
         public string Name { get; set; }
+        public virtual List<Competition> Competitions { get; set; } = new();
+        public virtual List<Sportsman> Sportsmen { get; set; } = new();
+
         public SportKind(string Name)
         {
             this.Name = Name;
         }
-        public virtual List<Competition> Competitions { get; set; } = new();
-        public virtual List<Sportsman> Sportsmen { get; set; } = new();
+
         public override string ToString()
         {
             return Name;
