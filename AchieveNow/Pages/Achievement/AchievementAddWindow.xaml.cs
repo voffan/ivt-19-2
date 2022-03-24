db0535ebd81f43000cfde32ae3fdcecec60b2930
ruslanrb <rabotenkoruslan@gmail.com>
2022-03-24 15:52:15 +0900

    changed ListOfAchievements

--- AchieveNow/Pages/Achievement/AchievementAddWindow.xaml.cs
+++ AchieveNow/Pages/Achievement/AchievementAddWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AchieveNow.Classes;
+using AchieveNow.ProgramClasses;
 
 namespace AchieveNow.Pages.Achievement
 {
@@ -24,9 +26,51 @@ namespace AchieveNow.Pages.Achievement
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = true;
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            ListOfCompetitions();
+        }
+
+        private void ListOfCompetitions()
+        {
+            Competition_ComboBox.Items.Clear();
+
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    var competitions = context.Competitions.ToList();
+                    foreach (Classes.Competition competition in competitions)
+                    {
+                        Competition_ComboBox.Items.Add(competition);
+                    }
+
+                    Competition_ComboBox.DisplayMemberPath = "Name";
+                    Competition_ComboBox.SelectedValuePath = "Id";     
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrorWindow showErrorWindow = new ShowErrorWindow();
+                showErrorWindow.ShowDialog();
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
