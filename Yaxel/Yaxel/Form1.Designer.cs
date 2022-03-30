209acafb46b914710fe7b960f00ba97e9d3372fd
NekoSoup <baiduolan2017@mail.ru>
2022-03-30 16:23:30 +0900

    Kolya lox

--- Yaxel/Yaxel/Form1.Designer.cs
+++ Yaxel/Yaxel/Form1.Designer.cs
@@ -31,7 +31,6 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.newNoteButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -65,16 +64,6 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(462, 400);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(203, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Посмотреть список периферии";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(671, 400);
@@ -102,7 +91,6 @@
             this.ClientSize = new System.Drawing.Size(886, 459);
             this.Controls.Add(this.newNoteButton);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -119,7 +107,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button newNoteButton;
     }
