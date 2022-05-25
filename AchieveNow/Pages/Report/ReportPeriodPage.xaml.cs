676bb008e5f44c4418ac888c7fb59860cd7a3ca1
sherwood2142 <sherwood2142@icloud.com>
2022-05-25 13:26:33 +0900

    almost made Period Report

--- AchieveNow/Pages/Report/ReportPeriodPage.xaml.cs
+++ AchieveNow/Pages/Report/ReportPeriodPage.xaml.cs
@@ -22,6 +22,10 @@ using AchieveNow.Pages.Sportsman;
 using AchieveNow.Pages.SportKind;
 using AchieveNow.Pages.Country;
 using AchieveNow.Pages.User;
+using MySql.Data.MySqlClient;
+using Microsoft.Data.Sqlite;
+using System.IO;
+using System.Text.Json;
 
 namespace AchieveNow.Pages.Report
 {
@@ -73,7 +77,6 @@ namespace AchieveNow.Pages.Report
                     return;
                 }
 
-                ShowGrid();
                 ListOfSportkinds();
             }
         }
@@ -126,7 +129,84 @@ namespace AchieveNow.Pages.Report
                 return;
             }
 
-            MessageBox.Show("Coming Soon!");
+
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                if (!context.IsAvailable)
+                    return;
+
+                List<ReportPeriodGrid> win = new List<ReportPeriodGrid>();
+                string sql = $"SELECT Sportsmen.Id, Sportsmen.Name, Achievements.Result, SportKinds.Name, Competitions.DateOfExecution, Competitions.Name AS \"Competition\", Achievements.DateOfIssue, Sportsmen.Gender, Countries.Name FROM SportKinds JOIN Sportsmen ON SportKinds.Id = Competitions.SportKindId JOIN Achievements ON Achievements.SportsmanId = Sportsmen.Id JOIN Competitions ON Competitions.Id = Achievements.CompetitionId JOIN Countries ON Countries.Id = Sportsmen.CountryId WHERE SportKinds.Id = {sportKindId} AND (Competitions.DateOfExecution >= '{fromDateOfExecution}' AND Competitions.DateOfExecution <= '{toDateOfExecution}') AND Achievements.Result = 3";
+
+                FileInfo configurationFile = new FileInfo("ConfigurationDB.json");
+                var configurationDB = JsonSerializer.Deserialize<ConfigurationDB>(File.ReadAllText(configurationFile.ToString()));
+
+                if (configurationDB.isRemote)
+                {
+                    // Подключение к MySQL требует проверки
+                    string connString = $"Server={configurationDB?.Server};Database={configurationDB?.Database};port=3306;User Id={configurationDB?.User};password={configurationDB?.Password}";
+
+                    using (var connection = new MySqlConnection(connString))
+                    {
+                        connection.Open();
+
+                        MySqlCommand command = new MySqlCommand(sql, connection);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows) // если есть данные
+                            {
+                                while (reader.Read())   // построчно считываем данные
+                                {
+                                    var id = reader.GetValue(0);
+                                    var name = reader.GetValue(1);
+                                    Result result = (Result)Enum.GetValues(typeof(Result)).GetValue(Convert.ToInt64(reader.GetValue(2)));
+                                    var sportKind = reader.GetValue(3);
+                                    var dateOfExecution = Convert.ToDateTime(reader.GetValue(4));
+                                    var competition = reader.GetValue(5);
+                                    var dateOfIssue = Convert.ToDateTime(reader.GetValue(6));
+                                    Gender gender = (Gender)Enum.GetValues(typeof(Gender)).GetValue(Convert.ToInt64(reader.GetValue(7)));
+                                    var country = reader.GetValue(8);
+
+                                    win.Add(new ReportPeriodGrid() { Id = id.ToString(), Name = name.ToString(), Result = result.ToString(), SportKind = sportKind.ToString(), DateOfExecution = DateOnly.FromDateTime(dateOfExecution), Competition = competition.ToString(), DateOfIssue = DateOnly.FromDateTime(dateOfIssue), Gender = gender.ToString(), Country = country.ToString() });
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    using (var connection = new SqliteConnection("Data Source=AchieveNowDB.db"))
+                    {
+                        connection.Open();
+
+                        SqliteCommand command = new SqliteCommand(sql, connection);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows) // если есть данные
+                            {
+                                while (reader.Read())   // построчно считываем данные
+                                {
+                                    var id = reader.GetValue(0);
+                                    var name = reader.GetValue(1);
+                                    //Result result = (Result)Enum.GetValues(typeof(Result)).GetValue(Convert.ToInt64(reader.GetValue(2)));
+                                    var result = reader.GetValue(2);
+                                    var sportKind = reader.GetValue(3);
+                                    var dateOfExecution = Convert.ToDateTime(reader.GetValue(4));
+                                    var competition = reader.GetValue(5);
+                                    var dateOfIssue = Convert.ToDateTime(reader.GetValue(6));
+                                    Gender gender = (Gender)Enum.GetValues(typeof(Gender)).GetValue(Convert.ToInt64(reader.GetValue(7)));
+                                    var country = reader.GetValue(8);
+
+                                    win.Add(new ReportPeriodGrid() { Id = id.ToString(), Name = name.ToString(), Result = result.ToString(), SportKind = sportKind.ToString(), DateOfExecution = DateOnly.FromDateTime(dateOfExecution), Competition = competition.ToString(), DateOfIssue = DateOnly.FromDateTime(dateOfIssue), Gender = gender.ToString(), Country = country.ToString() });
+                                }
+                            }
+                        }
+                    }
+                }
+                ReportPeriodGrid.ItemsSource = win;
+            }
         }
 
         public void Print_Button_Click(object sender, RoutedEventArgs e)
@@ -162,3 +242,28 @@ namespace AchieveNow.Pages.Report
         }
     }
 }
+
+public class ReportPeriodGrid
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Result { get; set; }
+    public string SportKind { get; set; }
+    public DateOnly DateOfExecution { get; set; }
+    public string Competition { get; set; }
+    public DateOnly DateOfIssue { get; set; }
+    public string Gender { get; set; }
+    public string Country { get; set; }
+}
+
+// 2022-05-25 12:55. Альтернативный вариант report если нужны баллы
+/*
+ SELECT *, row_number() over(order by mytable.Scores desc) as Place
+FROM (
+	SELECT Sportsmen.Id, Sportsmen.Name, SportKinds.Name, Sportsmen.Gender, Countries.Name, Competitions.DateOfExecution, (SUM(Achievements.Result)+SUM(Competitions.Level)) as "Scores"
+	FROM SportKinds JOIN Sportsmen ON SportKinds.Id = Competitions.SportKindId JOIN Achievements ON Achievements.SportsmanId = Sportsmen.Id JOIN Competitions ON Competitions.Id = Achievements.CompetitionId JOIN Countries ON Countries.Id = Sportsmen.CountryId
+	WHERE SportKinds.Id = 1 AND (Competitions.DateOfExecution >= '2022-02-01' AND Competitions.DateOfExecution <= '2022-04-31')
+	GROUP BY Sportsmen.Id
+) mytable
+ORDER BY mytable.Scores DESC
+ */
